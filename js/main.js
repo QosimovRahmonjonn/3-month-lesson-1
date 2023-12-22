dcc681854593da0a75846acac4f00dc467df47ba
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2023-12-22 21:40:05 +0500

    3-month lesson-1 homework

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// LIGHT DARK
+let modeBtn = document.getElementById("light-dark");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
+// NAVBAR RESPONSIVE
+
+function toggleNavbar() {
+  document.getElementById("navbar-responsive").classList.toggle("open");
+}
+
+document.getElementById("menu-btn").addEventListener("click", toggleNavbar);
